726e74f41c1cbbf491d47f3fbb7b52e44782187d
MrFluffycloud <aliadraseen@gmail.com>
2022-03-24 20:50:08 +0530

    🧪

--- components/Info/Testimonials.vue
+++ components/Info/Testimonials.vue
@@ -12,32 +12,64 @@
           Read some reviews from our beloved customers!
         </p>
       </div>
+        <div
+          class="grid grid-cols-1 gap-8 mt-16 sm:grid-cols-2 lg:grid-cols-3 sm:gap-16"
+        >
+          <!-- -->
+          <div v-for="user in users" :key="user">
+            <img
+              :src="user.icon"
+              :alt="user.name"
+              class="object-cover w-24 h-24 mx-auto rounded-full shadow-xl"
+            />
 
-      <div
-        class="grid grid-cols-1 gap-8 mt-16 sm:grid-cols-2 lg:grid-cols-3 sm:gap-16"
-      >
-        <!-- -->
-        <div>
-          <img
-            src="https://images-ext-1.discordapp.net/external/-FBv5hYELSTA3o45J8HV3dpzWyfzZ-4nwyby7iWz7qA/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/706908975907471362/5ce69325ebac79e752f0c2043f95ab05.png"
-            alt=""
-            class="object-cover w-24 h-24 mx-auto rounded-full shadow-xl"
-          />
-
-          <blockquote
-            class="flex flex-col justify-between p-12 -mt-6 text-center bg-gray-800 rounded-lg shadow-xl"
-          >
-            <p class="text-lg font-bold text-gray-100">! AB#2022</p>
-            <p class="mt-1 text-xs font-medium text-gray-300">
-              Owner of Wonder
-            </p>
-            <p class="mt-4 text-white text-medium">
-              WE COOl yk
-            </p>
-          </blockquote>
-        </div>
-        <!-- -->
+            <blockquote
+              class="flex flex-col justify-between p-12 -mt-6 text-center bg-gray-800 rounded-lg shadow-xl"
+            >
+              <p class="text-lg font-bold text-gray-100">{{ user.name }}</p>
+              <p class="mt-1 text-xs font-medium text-gray-300">
+                {{ user.role }}
+              </p>
+              <p class="mt-4 text-white text-medium">{{ user.message }}</p>
+            </blockquote>
+          </div>
+          <!-- -->
       </div>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          name: '! AB#2022',
+          icon: 'https://cdn.discordapp.com/avatars/706908975907471362/5ce69325ebac79e752f0c2043f95ab05.png',
+          role: 'Owner of Wonder',
+          message: 'WE COOl yk',
+        },
+        {
+          name: '! AB#2022',
+          icon: 'https://cdn.discordapp.com/avatars/706908975907471362/5ce69325ebac79e752f0c2043f95ab05.png',
+          role: 'Owner of Wonder',
+          message: 'WE COOl yk',
+        },
+        {
+          name: '! AB#2022',
+          icon: 'https://cdn.discordapp.com/avatars/706908975907471362/5ce69325ebac79e752f0c2043f95ab05.png',
+          role: 'Owner of Wonder',
+          message: 'WE COOl yk',
+        },
+        {
+          name: '! AB#2022',
+          icon: 'https://cdn.discordapp.com/avatars/706908975907471362/5ce69325ebac79e752f0c2043f95ab05.png',
+          role: 'Owner of Wonder',
+          message: 'WE COOl yk',
+        },
+      ],
+    }
+  },
+}
+</script>
