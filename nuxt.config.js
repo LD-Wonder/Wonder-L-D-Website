d7902a339e0bfa6032b8ed4328642ea8fc1bad0a
MrFluffycloud <aliadraseen@gmail.com>
2022-04-02 16:03:30 +0530

    🧪

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,44 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'sad' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Rapid Services, a place to promote your stuff like no other!',
+      },
       { name: 'format-detection', content: 'telephone=no' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'og:url', content: 'https://rapid-services.org' },
+      {
+        name: 'og:image',
+        content: 'https://imgs.mrfluffycloud.xyz/Logo-Bolt-Resized.png',
+      },
+      { name: 'og:title', content: 'Rapid Services' },
+      { name: 'og:site_name', content: 'Rapid Services' },
+      {
+        name: 'og:description',
+        content: 'Rapid Services, a place to promote your stuff like no other!',
+      },
+      { name: 'author', content: 'MrFluffycloud' },
+      { name: 'theme-color', content: '#9333ea' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'shortcut-icon',
+        href: 'https://imgs.mrfluffycloud.xyz/Logo-Bolt-Resized-96.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '128x128',
+        href: 'https://imgs.mrfluffycloud.xyz/Logo-Bolt-Resized-128.png',
+      },
+      {
+        rel: 'mask-icon',
+        color: '#9333ea',
+        href: 'https://imgs.mrfluffycloud.xyz/Logo.svg',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
